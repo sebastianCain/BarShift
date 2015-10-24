4dfd0dc4f6a07a76de238e79b743f52e1fbaeacb
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2015-10-24 14:41:53 -0400

    init

---
+++ BarShift/BarShiftScene.swift
@@ -0,0 +1,13 @@
+//
+//  BarShiftScene.swift
+//  BarShift
+//
+//  Created by Sebastian Cain on 10/24/15.
+//  Copyright © 2015 Isometric LLC. All rights reserved.
+//
+
+import SpriteKit
+
+class BarShiftScene: SKScene {
+
+}
